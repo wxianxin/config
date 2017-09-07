5be5938bab0c1c2914c470692767d7b0a64c7f9b
wxianxin <wxianxinreg@gmail.com>
2017-09-06 23:23:44 -0400

    20170906

--- ubuntu_16.04.3_install_clean.sh
+++ ubuntu_16.04.3_install_clean.sh
@@ -18,11 +18,11 @@ sudo apt purge empathy*
 
 sudo apt autoremove
 sudo apt update
-#sudo apt install openssh-server
-#sudo apt install transmission-daemon
 sudo apt install vim
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-all google-chrome-stable_current_amd64.deb
+#sudo apt install openssh-server
+#sudo apt install transmission-daemon
 # sudo apt-get install mplayer
 # sudo apt-get install fcitx-googlepinyin
 
@@ -35,5 +35,4 @@ sudo reboot
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 # /etc/rc.local
-
 echo 10 > /sys/class/backlight/intel_backlight/brightness
