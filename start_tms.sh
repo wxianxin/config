17bbefbf7a5a1a975448d68c95001cf4f7af1793
wxianxin <wxianxinreg@gmail.com>
2020-05-01 00:33:10 -0400

    add python file server

--- start_tms.sh
+++ start_tms.sh
@@ -6,3 +6,4 @@ transmission-daemon
 
 sudo mount -o rsize=32768,wsize=32768 192.168.1.1:/home/pi/L ~/nfs
 rsync -aP -e "ssh -p 22" /home/coupe/L/Downloads/blabla pi@192.168.1.1:/home/pi/L/Downloads
+python -m http.server 8000
