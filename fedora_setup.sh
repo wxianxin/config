9e8d455b8ffcbd60a349f1c0bb79948e7b3b162e
wxianxin <wxianxinreg@gmail.com>
2020-06-12 11:54:52 -0400

    update for fedora 32

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -2,19 +2,17 @@ echo "########">> ~/.bashrc
 echo "# Steven">> ~/.bashrc
 echo "alias python="python3"">> ~/.bashrc
 echo "alias dropbox=~/.dropbox-dist/dropboxd">> ~/.bashrc
-echo "alias tms='transmission-remote --auth transmission:password'" ~/.bashrc
+echo "alias tms='transmission-remote --auth transmission:password'" >> ~/.bashrc
 echo "source ~/.venvs/p/bin/activate" >> ~/.bashrc
 
 cp -r ~/config/.vim ..
-cp ~/.config/.vimrc ..
-cp ~/.config/.toprc ..
+cp ~/config/.vimrc ..
+cp ~/config/.toprc ..
 
 sudo dnf install vim
 # sudo dnf install mplayer
 # sudo dnf install openssh-server
 # sudo dnf install transmission-daemon
-# sudo dnf install ibus-pinyin
-# sudo dnf install fcitx-googlepinyin
 
 sudo dnf autoremove
 
@@ -23,3 +21,8 @@ sudo dnf autoremove
 # cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 # sudo mount /dev/nvme0n1p5 /home/coupe/L/
+
+########################################################################################
+# Dell G5 SE 
+# sudo grubby --update-kernel=ALL --args="amdgpu.runpm=0"
+########################################################################################
