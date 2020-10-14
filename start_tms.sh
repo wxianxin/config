c8dc990bbab82fd44b6c3239a5a869aafe37c448
wxianxin <wxianxinreg@gmail.com>
2020-10-14 19:19:23 -0400

    update mount ftp

--- start_tms.sh
+++ start_tms.sh
@@ -1,6 +1,8 @@
 # sudo mount /dev/nvme0n1p5 ~/L
 # sudo mount -o rsize=32768,wsize=32768 192.168.1.1:/home/pi/L ~/nfs
 sudo mount -t cifs -o username=sysxadmin,uid=coupe,vers=2.0 //192.168.50.1/drive ~/L/Downloads
+# sudo apt install curlftpfs
+# curlftpfs -o user={$USERNAME}:{$PASSWORD} 192.168.50.1/Seagate_Backup_Plus_Drive/drive ~/L/Downloads
 
 # restart the service because it won't read user specific settings right after boot
 sudo service transmission-daemon stop
