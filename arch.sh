8242d6e249ca6196b486fb4e8381d526fe0443d0
wxianxin <wxianxinreg@gmail.com>
2022-08-31 11:54:57 +0000

    add nmcli connect wifi

--- arch.sh
+++ arch.sh
@@ -8,6 +8,7 @@ systemctl enable NetworkManager.service
 
 # wifi
 sudo systemctl start iwd & sudo iwctl station wlan0 connect Italia_5G & sudo dhcpcd wlan0
+# sudo nmcli dev wifi connect Italia_5G password ""
 ## bluetooth
 sudo systemctl start bluetooth.service
 
