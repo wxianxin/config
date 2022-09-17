0012bf64cfaaf1c4cc65008f00c249649a8a4cc7
wxianxin <wxianxinreg@gmail.com>
2022-09-17 22:55:32 +0000

    udpate arch install

--- arch.sh
+++ arch.sh
@@ -1,14 +1,34 @@
-pacman -S os-prober, bluez, bluez-utils, networkmanager
-# pacman -S noto-fonts-cjk
+# arch install
+
+ls -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+echo archname > /etc/hostname
+pacman -S grub efibootmgr os-prober
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+genfstab -U /mnt >> /mnt/etc/fstab
+# add user other than root
+useradd -m -s /bin/bash my_user
+# enable sudo for the user
+usermod -aG wheel my_user
+
+pacman -S networkmanager sudo
+
+pacman -S gdm gnome-terminal
+pacman -S gnome-control-center
+pacman -S nautilus
+# pacman -S gvfs-smb # smb for nautilus
+
+pacman -S bluez bluez-utils
+pacman -S noto-fonts-cjk    # chinese font
 
 # autostart
-# systemctl enable iwd
 systemctl enable gdm.service
 systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
 
 # wifi
 sudo systemctl start iwd & sudo iwctl station wlan0 connect Italia_5G & sudo dhcpcd wlan0
 # sudo nmcli dev wifi connect Italia_5G password ""
-## bluetooth
-sudo systemctl start bluetooth.service
 
