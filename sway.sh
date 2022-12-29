9b0084cfe922672f38cd1efa9acd518e1a0a0d1b
wxianxin <wxianxinreg@gmail.com>
2022-12-28 22:44:53 -0500

    update sway

--- sway.sh
+++ sway.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # sway
 
-sudo pacman -S sway foot
+sudo pacman -S sway swaybg swayidle swaylock foot 
 
-mkdir .config/sway
-# mkdir -p .config/sway
+mkdir -p .config/sway
 cp /etc/sway/config .config/sway
+cp ~/config/sway/* .config/sway
 
-# .config/sway/config
-cat >> ~/.config/sway/config << EOF
+# Add single or double quotes to the first delimiter to ignore variable and command expansion in a HereDoc
+cat >> ~/.config/sway/config << "EOF"
 ########################################################################################
+# Steven
+output * bg ~/config/static/colorful.jpg fill 
+
 input * accel_profile flat
+input * pointer_accel -0.5
+
+bindsym $mod+0 exec bash ~/.config/sway/lock.sh 
+
+exec swayidle -w \
+         timeout 480 'swaylock -f -c 000000' \
+         timeout 490 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
+         before-sleep 'swaylock -f -c 000000'
 EOF
 
 # Add single or double quotes to the first delimiter to ignore variable and command expansion in a HereDoc
@@ -25,3 +36,20 @@ if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
 fi
 EOF
 
+
+
+# ~/.config/sway/config
+# bar {
+#     position top
+#     mode hide
+# 
+#     # When the status_command prints a new line to stdout, swaybar updates.
+#     # The default just shows the current date and time.
+#     status_command while date +'%Y-%m-%d %H:%M:%S'; do sleep 1; done
+# 
+#     colors {
+#         statusline #00ff00
+#         background #000000
+#         inactive_workspace #32323200 #32323200 #5c5c5c
+#     }
+# }
---
+++ sway/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+swayidle \
+    timeout 3 'swaymsg "output * dpms off"' \
+        resume 'swaymsg "output * dpms on"' &
+# Locks the screen immediately
+swaylock -c 550000
+# Kills last background task so idle timer doesn't keep running
+kill %%
