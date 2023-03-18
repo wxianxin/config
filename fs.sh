371e60074a6ded765792c06a0e10e0b8c2d05247
wxianxin <wxianxinreg@gmail.com>
2023-03-17 23:40:40 -0400

    add file server script

---
+++ fs.sh
@@ -0,0 +1,7 @@
+sudo cryptsetup -v luksOpen /dev/nvme0n1p1 L
+sudo mount /dev/mapper/L drive
+sudo chown -R coupe:coupe /dev/mapper/L
+
+sudo nmcli dev disconnect wlo1
+
+sudo systemctl start target.service
