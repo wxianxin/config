f491843ff8c0a1192d6418568002dddbf6d03798
wxianxin <wxianxinreg@gmail.com>
2023-03-19 21:04:01 -0400

    update file client

--- fc.sh
+++ fc.sh
@@ -1,2 +1,3 @@
 sudo mount -o rsize=32768,wsize=32768 192.168.50.64:/home/coupe/drive ~/L
+sudo systemctl start iscsid.service
 sudo iscsiadm --mode node --targetname iqn.2023-03.z.y.x.n6000:drive --portal 192.168.50.64 --login
