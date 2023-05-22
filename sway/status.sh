3a8b8e651de1a4ea4e5d83797c50ce9a2616e270
wxianxin <wxianxinreg@gmail.com>
2023-05-22 10:46:35 +0000

    update

--- sway/status.sh
+++ sway/status.sh
@@ -15,7 +15,7 @@ volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed -n '$s/.*\(..\)$/\1/p')
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 
 # emojis
-# 🖩🖴 💾📝😀⏳⏱️⏰⌚
+# 🖩🖴 💾📝😀⏳⏱️⏰⌚💻
 
-echo 🌡️$cpu_temp ☢️ $fan_speed 🕒$uptime_formatted 💻$mem_usage 🔊$volume% $datetime 🔋$battery%
+echo 🌡️$cpu_temp ☢️ $fan_speed 🕒$uptime_formatted 💾$mem_usage 🔊$volume% $datetime 🔋$battery%
 
