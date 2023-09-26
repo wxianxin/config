acf15f6a519e9a1e31eac0ba4cfc9f60161601ca
wxianxin <wxianxinreg@gmail.com>
2023-09-26 14:44:10 -0400

    update

--- fs.sh
+++ fs.sh
@@ -2,7 +2,8 @@ sudo cryptsetup -v luksOpen /dev/nvme0n1p1 L
 sudo mount /dev/mapper/L drive
 sudo chown -R $USER:$USER /dev/mapper/L
 
-sudo nmcli dev disconnect wlo1
+# sudo nmcli dev disconnect wlo1
+sudo ip link set wlan0 down
 
 sudo systemctl start nfs-server.service
 sudo systemctl start target.service
