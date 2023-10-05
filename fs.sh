38456bb352ecfd5ae2d822d6af7cc95db4e98a1f
wxianxin <wxianxinreg@gmail.com>
2023-10-05 16:24:39 -0400

    update

--- fs.sh
+++ fs.sh
@@ -8,12 +8,12 @@ sudo ip link set wlan0 down
 sudo systemctl start nfs-server.service
 sudo systemctl start target.service
 sudo systemctl start docker.service
-sudo docker compose -f config/docker-compose.yaml up -d
+# sudo docker compose -f config/docker-compose.yaml up -d
 
 # backup job
-sudo cryptsetup -v luksOpen /dev/sdc1 B
+sudo cryptsetup -v luksOpen /dev/sdb1 B
 sudo mount /dev/mapper/B mnt
 sudo chown -R $USER:$USER /dev/mapper/B
-rsync -ahzP --progress /home/$USER/drive/ /home/$USER/mnt/backup/drive/ | tee /home/$USER/mnt/backup/backup.log
+rsync -ahzP --progress --exclude 'downloads' /home/$USER/drive/ /home/$USER/mnt/backup/drive/ 2>&1 | tee /home/$USER/mnt/backup/backup.log
 sudo umount /home/$USER/mnt
 sudo cryptsetup luksClose /dev/mapper/B
