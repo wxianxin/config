ccfa79db6f3ccd5c2e6bcf5a5036c65a264e92e2
wxianxin <wxianxinreg@gmail.com>
2023-10-12 14:16:06 +0100

    update

--- customize.sh
+++ customize.sh
@@ -4,10 +4,12 @@
 cat >> ~/.bashrc << EOF
 ########################################################################################
 # Steven
-PS1='\[\e]0;\w\a\]\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\$ '
+# PS1='\[\e]0;\w\a\]\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\$ '
 alias python="python3"
 alias weather="curl 'wttr.in/?m&format=%T+%l+%C+%c+%t+%h+%w+%m+%M+%p+%P+%u\nDawn:+%D\nSunrise:+%S\nZenith:+%z\nSunset:+%s\nDusk:+%d\n'"
 source ~/.venv/p/bin/activate
+export TERM=xterm-256color
+eval "$(starship init bash)"
 EOF
 
 source ~/.bashrc
@@ -27,17 +29,17 @@ python -m pip install numpy pandas black flake8    # flake8 for vim ale
 
 # rustup component add rls, rust-analysis, rust-src   # needed for vim etc.
 ########################################################################################
-# Gsettings
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-## HIDPI
-gsettings set org.gnome.desktop.interface scaling-factor 2
-## set wallpaper
-gsettings get org.gnome.desktop.background picture-uri  # print current wallpaper path
-gsettings set org.gnome.desktop.background picture-uri file:///~/config/config/colorful.jpg
-## Set keyboard shortcut for terminal
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'gnome-terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
+# # Gsettings
+# gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+# ## HIDPI
+# gsettings set org.gnome.desktop.interface scaling-factor 2
+# ## set wallpaper
+# gsettings get org.gnome.desktop.background picture-uri  # print current wallpaper path
+# gsettings set org.gnome.desktop.background picture-uri file:///~/config/config/colorful.jpg
+# ## Set keyboard shortcut for terminal
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'gnome-terminal'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
 ########################################################################################
 # git
 git config --global diff.tool vimdiff
